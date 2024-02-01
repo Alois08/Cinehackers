1851934d70164ce580be0c5e2baefbb33206a4b1
Alois08 <144676725+Alois08@users.noreply.github.com>
2024-02-01 09:50:00 +0100

    Update Prepa_app_streamlit_final.py

--- Prepa_app_streamlit_final.py
+++ Prepa_app_streamlit_final.py
@@ -96,7 +96,7 @@ def onglet0():
     # Sous-titre
     st.markdown("<p class='medium-font'>Le cinéma Cinetflix passe le cap du numérique !!!</p>", unsafe_allow_html=True)
     old_film = "DALLE_2023-11-28_15.56.08_-_A_cinema_transitioning_from_traditional_to_digital_format._The_scene_includes_a_classic_film_projector_and_film_reels_on_one_side_symbolizing_the_old.webp"
-    url2 = 'https://drive.google.com/uc?export=view&id=1e57cwl_8Pv_733qiClKvLpF_QVA2FvZP'
+    url2 = 'cinehackers img1.png'
     st.image(old_film, width=700)
     st.markdown('<p class="small-font">Situé dans la Creuse, Cinetflix franchit une nouvelle étape  technologique. Mandaté par Cinetflix, CineHackers vous propose une application de recommandation de films ainsi que quelques indicateurs sur la base de données mise à disposition.</p>', unsafe_allow_html=True)
     
@@ -597,7 +597,7 @@ def translate_text(text):
     
     
 def main():
-    url2 = 'https://drive.google.com/uc?export=view&id=1e57cwl_8Pv_733qiClKvLpF_QVA2FvZP'
+    url2 = 'cinehackers img1.png'
     st.sidebar.image(url2)
 
     st.sidebar.title("Navigation")
